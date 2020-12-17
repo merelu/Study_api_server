ed12241bf9d00c1d7fe887e1d2a81202ece65649
GYUHA_HWANG <gyuha0310@gmail.com>
2020-12-17 22:16:32 +0900

    cors 패키지 설치 및 v2.js 라우터에 적용 nodecat main뷰 작성

--- nodecat/routes/index.js
+++ nodecat/routes/index.js
@@ -63,4 +63,8 @@ router.get("/test", async (req, res, next) => {
   }
 });
 
+router.get("/", (req, res) => {
+  res.render("main", { key: process.env.CLIENT_SECRET });
+});
+
 module.exports = router;
